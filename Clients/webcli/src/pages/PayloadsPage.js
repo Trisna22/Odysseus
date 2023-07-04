3c365ed6cc95d8daaa25b1e9ad3887d80715072f
gedetrt <trisna.quebe@hva.nl>
2023-07-04 23:32:25 +0200

    Added payload designs

--- Clients/webcli/src/pages/PayloadsPage.js
+++ Clients/webcli/src/pages/PayloadsPage.js
@@ -1,7 +1,7 @@
-import { useContext, useEffect, useState } from "react";
-import { Accordion, AccordionContext, Badge, Button, Card, Col, Container, Row, Spinner, useAccordionButton } from "react-bootstrap";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Accordion, AccordionContext, Badge, Button, Card, Col, Container, Form, InputGroup, Modal, Row, Spinner, useAccordionButton } from "react-bootstrap";
 import UserService from "../services/UserService";
-import {FaTrashAlt} from "react-icons/fa"
+import {FaBomb, FaTrashAlt} from "react-icons/fa"
 
 const PayloadsPage = () => {
 
@@ -9,8 +9,58 @@ const PayloadsPage = () => {
     const [payloads, setPayloads] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    // Upload payload.
+    const [modalPayloadShow, setModalPayloadShow] = useState(false);
+    const inputRef = useRef();
+    const [uploadedFile, setUploadedFile] = useState("");
+    const onFileChange = () => {
+        inputRef.current?.files && 
+        setUploadedFile(inputRef.current.files[0].name)       
+    }
+
+    // Delete payload.
+    const [modalDeletePayloadShow, setModalDeletePayloadShow] = useState(false);
+    const [payloadToDelete, setPayloadToDelete] = useState({});
+    const confirmDelete = (index) => {
+        setPayloadToDelete(payloads[index]);
+        setModalDeletePayloadShow(true);
+    }
+    const deletePayload = () => {
+        setModalDeletePayloadShow(false);
+        alert("Deleting payload with id " + payloadToDelete.id)
+    }
+
     // Accordion magic.
-    const [activeId, setActiveId] = useState(0);
+    const [activeId, setActiveId] = useState(0);    
+    const badgeColor = (category) => {
+
+        switch (category) {
+            case 'haunted':
+            case 'scary':
+                return 'danger'
+            
+            case 'stealth':
+                return 'secondary'
+            
+            case 'command & control':
+                return "warning";
+
+            default:
+                return 'primary'
+        }
+    }
+    const CustomAccordion = ({children, eventKey}) => {
+
+        const decoratedOnClick = useAccordionButton(eventKey)
+
+        return (
+            <>
+                <a style={{cursor: "pointer", textDecorationLine: "underline"}} 
+                    onClick={decoratedOnClick}>{children}</a>
+                <FaTrashAlt style={{float: "right", cursor: "pointer"}} onClick={() =>confirmDelete(eventKey) }/>
+            </>
+        )
+    }
 
     useEffect(() => {
         setInterval(() => {
@@ -31,49 +81,61 @@ const PayloadsPage = () => {
         }, 1000)
     }, [])
     
-    const badgeColor = (category) => {
-
-        switch (category) {
-            case 'haunted':
-            case 'scary':
-                return 'danger'
-            
-            case 'stealth':
-                return 'secondary'
-            
-            case 'command & control':
-                return "warning";
-
-            default:
-                return 'primary'
-        }
-    }
-
-    const deletePayload = (index) => {
-        alert("Deleting payload with index " + index)
-    }
-
-    const CustomAccordion = ({children, eventKey}) => {
-
-        const decoratedOnClick = useAccordionButton(eventKey)
-
-        return (
-            <>
-                <a style={{cursor: "pointer", textDecorationLine: "underline"}} 
-                    onClick={decoratedOnClick}>{children}</a>
-                <FaTrashAlt style={{float: "right", cursor: "pointer"}} onClick={() => deletePayload(eventKey)}/>
-            </>
-        )
-    }
-    
     return (
         <Container>
+            <Modal show={modalPayloadShow} onHide={() => setModalPayloadShow(false)}>
+                <Modal.Header closeButton>
+                    <h3><FaBomb/> New payload</h3>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup>
+                        <InputGroup.Text>Name</InputGroup.Text>
+                        <Form.Control placeholder="Self destruction payload"></Form.Control>
+                    </InputGroup>   
+                    <br/>
+                    <Form>
+                        <Form.Label>Description of payload</Form.Label>
+                        <Form.Control placeholder="Does haxx" as="textarea" rows={3} />                    
+                    </Form>
+                    <br/>
+                    <InputGroup>
+                        <InputGroup.Text>Variables</InputGroup.Text>
+                        <Form.Control placeholder="TARGET_IP"></Form.Control>
+                        <Button variant="outline-secondary">Extra variables</Button>
+                    </InputGroup>
+                    <br/>
+                    Choose file: <Button type="button" variant="outline-primary" 
+                        onClick={() => inputRef.current?.click()}>
+                            {uploadedFile ? uploadedFile : "Upload"}
+                    </Button>
+                    <input type="file" style={{display: "none"}} accept=".cpp,.c,.h,.hpp" 
+                            onChange={onFileChange} ref={inputRef}></input>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success">Upload</Button>
+                    <Button variant="warning" onClick={() => setModalPayloadShow(false)}>Cancel</Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={modalDeletePayloadShow} onHide={() => setModalDeletePayloadShow(false)} closeButton>
+                <Modal.Header>
+                    Warning
+                </Modal.Header>
+                <Modal.Body>
+                    Are you sure to delete the payload '{payloadToDelete.name}'?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success" onClick={() => deletePayload()}>Yes</Button>
+                    <Button variant="warning" onClick={() => setModalDeletePayloadShow(false)}>No</Button>
+                </Modal.Footer>
+            </Modal>
+
             <Row>
                 <Col>
             <h1>All payloads</h1> 
                 </Col>
                 <Col>
-                <Button variant="outline-success">Upload payload</Button>
+                <Button variant="outline-success" onClick={() => setModalPayloadShow(true)}>Upload payload</Button>
                 </Col>
             </Row>
 
