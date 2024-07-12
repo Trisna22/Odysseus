67113de2f1668bd916d330afa2791bd6b9922e97
Trisna22 <trisna@live.nl>
2024-07-12 19:05:25 +0200

    First push

---
+++ Implants/Components/SysWhispers/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <Windows.h>
+
+int main(int argc, char* argv[]) {
+
+
+	return 0;
+}
+
+/*
+	Resources:
+
+	https://www.ired.team/offensive-security/defense-evasion/using-syscalls-directly-from-visual-studio-to-bypass-avs-edrs
+	https://redops.at/en/blog/direct-syscalls-a-journey-from-high-to-low
+	https://www.outflank.nl/blog/2019/06/19/red-team-tactics-combining-direct-system-calls-and-srdi-to-bypass-av-edr/
+	https://medium.com/@merasor07/av-edr-evasion-using-direct-system-calls-user-mode-vs-kernel-mode-fad2fdfed01a
+	https://www.paloaltonetworks.com/blog/security-operations/a-deep-dive-into-malicious-direct-syscall-detection/
+*/
